1e317bb2ef2593c9f88fc911a4fb17d384ba461b
545857463@qq.com <Yip134679>
2015-08-09 10:34:49 +0800

    Yogi

---
+++ HOT/HotDoor/app/src/main/java/com/hotdoor/list/CmpListItem.java
@@ -0,0 +1,26 @@
+package com.hotdoor.list;
+
+/**
+ * Created by Yip on 2015/8/5.
+ */
+public class CmpListItem {
+    public int imageId;
+    public String text;
+
+    public int getImageId() {
+        return imageId;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setImageId(int imageId) {
+
+        this.imageId = imageId;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
--- HOT/HotDoor/app/src/main/java/com/hotdoor/products/main/CompanyActivity.java
+++ HOT/HotDoor/app/src/main/java/com/hotdoor/products/main/CompanyActivity.java
@@ -2,16 +2,60 @@ package com.hotdoor.products.main;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ListView;
+
+import com.hotdoor.adapter.CmpAdapter;
+import com.hotdoor.list.CmpListItem;
+
+import java.util.ArrayList;
 
 /**
  * Created by Yip on 2015/8/3.
  */
-public class CompanyActivity extends Activity {
+public class CompanyActivity extends Activity implements View.OnClickListener {
+    private final static String[] CMP_TEXT = {"公司介绍", "专利证书", "公司荣誉", "合作伙伴", "联系我们"};
+    private final static int[] CMP_ICON = {R.drawable.testimage, R.drawable.testimage, R.drawable.testimage,
+            R.drawable.testimage, R.drawable.testimage};
+
+    ImageView mCmpLeftImg;
+    ListView listCompany;
+    ArrayList<CmpListItem> mCmpList = new ArrayList<>();
+    CmpAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.company);
+        initList();
+        init();
+    }
 
+    private void init() {
+        /** Initialize widgets */
+        mCmpLeftImg = (ImageView) findViewById(R.id.ic_company_left);
+        listCompany = (ListView) findViewById(R.id.list_campany);
+
+        mAdapter = new CmpAdapter(this, mCmpList);
+        listCompany.setAdapter(mAdapter);
+
+        mCmpLeftImg.setOnClickListener(this);
+    }
+
+    private void initList() {
+        for (int i = 0; i < CMP_TEXT.length; i++) {
+            CmpListItem mItem = new CmpListItem();
+            mItem.setImageId(CMP_ICON[i]);
+            mItem.setText(CMP_TEXT[i]);
+            mCmpList.add(mItem);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.ic_company_left) {
+            this.finish();
+        }
     }
 }
