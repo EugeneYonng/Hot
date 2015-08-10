98508b314f973a5e6d1c84332b6456f7b96c261b
545857463@qq.com <Yip134679>
2015-08-10 10:12:37 +0800

    Yogi

--- HOT/HotDoor/app/src/main/java/com/hotdoor/products/main/MainActivity.java
+++ HOT/HotDoor/app/src/main/java/com/hotdoor/products/main/MainActivity.java
@@ -3,62 +3,47 @@ package com.hotdoor.products.main;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Color;
-import android.graphics.drawable.RippleDrawable;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.andexert.library.RippleView;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.Timer;
 import java.util.TimerTask;
-
-import java.util.logging.LogRecord;
 
 import me.relex.circleindicator.CircleIndicator;
 
 
-public class MainActivity extends Activity{
-    public static final String ACTION_MAINACTIVITY="com.hotdoor.products.main.MainActivity";
+public class MainActivity extends Activity {
+    public static final String ACTION_MAINACTIVITY = "com.hotdoor.products.main.MainActivity";
     public static final String ACTION_COMPANY = "com.hotdoor.products.main.CompanyActivity";
     public static final String ACTION_PRODUCT = "com.hotdoor.products.main.ProductActivity";
     public static final String ACTION_METHOD = "com.hotdoor.products.main.MethodActivity";
     public static final String ACTION_PERSONAL = "com.hotdoor.products.main.PersonalActivity";
     public static final String ACTION_SERVICE = "com.hotdoor.products.main.ServiceActivity";
 
-    private ViewPager mainPager;
-    private RippleView companyRipp;
-    private RippleView productRipp;
-    private RippleView methodRipp;
-    private RippleView personRipp;
-    private RippleView serverRipp;
+    private ViewPager pagerMain;
+    private RippleView rippCompany;
+    private RippleView rippProduct;
+    private RippleView rippMethod;
+    private RippleView rippPersonal;
+    private RippleView rippServer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        mainPager = (ViewPager)findViewById(R.id.vp_main);
-        companyRipp = (RippleView)findViewById(R.id.rv_company);
-        productRipp = (RippleView)findViewById(R.id.rv_product);
-        methodRipp = (RippleView)findViewById(R.id.rv_method);
-        personRipp = (RippleView)findViewById(R.id.rv_person);
-        serverRipp = (RippleView)findViewById(R.id.rv_server);
+        init();
 
-        mainPager.setAdapter(new PagerAdapter() {
+        pagerMain.setAdapter(new PagerAdapter() {
             @Override
             public int getCount() {
                 return 3;
@@ -94,31 +79,40 @@ public class MainActivity extends Activity{
         });
 
         CircleIndicator indicator = (CircleIndicator)findViewById(R.id.ci_main);
-        indicator.setViewPager(mainPager);
-        companyRipp.setOnRippleCompleteListener(new myListener());
-        productRipp.setOnRippleCompleteListener(new myListener());
-        methodRipp.setOnRippleCompleteListener(new myListener());
-        personRipp.setOnRippleCompleteListener(new myListener());
-        serverRipp.setOnRippleCompleteListener(new myListener());
+        indicator.setViewPager(pagerMain);
+        rippCompany.setOnRippleCompleteListener(new myListener());
+        rippProduct.setOnRippleCompleteListener(new myListener());
+        rippMethod.setOnRippleCompleteListener(new myListener());
+        rippPersonal.setOnRippleCompleteListener(new myListener());
+        rippServer.setOnRippleCompleteListener(new myListener());
 
-        new Timer().schedule(new myTimeTask(),0,4000);
+        new Timer().schedule(new myTimeTask(), 0, 4000);
     }
 
-    private class myTimeTask extends TimerTask{
+    private void init() {
+        pagerMain = (ViewPager) findViewById(R.id.vp_main);
+        rippCompany = (RippleView) findViewById(R.id.rv_company);
+        rippProduct = (RippleView) findViewById(R.id.rv_product);
+        rippMethod = (RippleView) findViewById(R.id.rv_method);
+        rippPersonal = (RippleView) findViewById(R.id.rv_person);
+        rippServer = (RippleView) findViewById(R.id.rv_server);
+    }
+
+    private class myTimeTask extends TimerTask {
 
         @Override
-        public void run(){
-            myhandler.sendEmptyMessage(0);
+        public void run() {
+            mHandler.sendEmptyMessage(0);
         }
     }
 
-    Handler myhandler = new Handler() {
+    Handler mHandler = new Handler() {
         private int index=0;
         @Override
-        public void handleMessage(Message msg){
+        public void handleMessage(Message msg) {
             if(index > 2)
                 index = 0;
-            mainPager.setCurrentItem(index);
+            pagerMain.setCurrentItem(index);
             index++;
         }
     };
@@ -142,7 +136,6 @@ public class MainActivity extends Activity{
                 case R.id.rv_server:
                     startIntent(ACTION_SERVICE);
                     break;
-
             }
         }
     }
--- HOT/HotDoor/app/src/main/java/com/hotdoor/products/main/MethodActivity.java
+++ HOT/HotDoor/app/src/main/java/com/hotdoor/products/main/MethodActivity.java
@@ -18,69 +18,72 @@ public class MethodActivity extends Activity {
     private ImageView methodLeft;
     private ImageView methodIcon;
     private MyTextView methodTitle;
-    private RelativeLayout smartCompany;
-    private RelativeLayout smartCampus;
-    private RegularHexagonView infoSafe;
-    private RegularHexagonView vallInfo;
-    private RegularHexagonView moveWork;
-    private RegularHexagonView holtInfo;
-    private RegularHexagonView finaInfo;
-    private RegularHexagonView libiInfo;
-    private RegularHexagonView induInfo;
+    private RelativeLayout mSmartCmp;
+    private RelativeLayout mSmartCampus;
+    private RegularHexagonView mSecurityInfo;
+    private RegularHexagonView mRuralInfo;
+    private RegularHexagonView mMoveWork;
+    private RegularHexagonView mHotelInfo;
+    private RegularHexagonView mFinanceInfo;
+    private RegularHexagonView mLibInfo;
+    private RegularHexagonView mIndustryInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.method);
 
-        methodLeft = (ImageView)findViewById(R.id.iv_methodLeft);
-        methodIcon = (ImageView)findViewById(R.id.iv_methonIcon);
-        methodTitle = (MyTextView)findViewById(R.id.mtv_methodTitle);
-        smartCampus = (RelativeLayout)findViewById(R.id.rl_smartCamp);
-        smartCompany = (RelativeLayout)findViewById(R.id.rl_smartComp);
-        infoSafe = (RegularHexagonView)findViewById(R.id.rhv_infoSafe);
-        vallInfo = (RegularHexagonView)findViewById(R.id.rhv_vallInfo);
-        moveWork = (RegularHexagonView)findViewById(R.id.rhv_moveWork);
-        holtInfo = (RegularHexagonView)findViewById(R.id.rhv_holtInfo);
-        finaInfo = (RegularHexagonView)findViewById(R.id.rhv_finaInfo);
-        libiInfo = (RegularHexagonView)findViewById(R.id.rhv_libiInfo);
-        induInfo = (RegularHexagonView)findViewById(R.id.rhv_induInfo);
+        init();
+    }
+
+    private void init() {
+        methodLeft = (ImageView) findViewById(R.id.iv_method_left);
+        methodIcon = (ImageView) findViewById(R.id.iv_method_left_icon);
+        methodTitle = (MyTextView) findViewById(R.id.tv_method_title);
+        mSmartCampus = (RelativeLayout) findViewById(R.id.rl_smart_campus);
+        mSmartCmp = (RelativeLayout) findViewById(R.id.rl_smart_company);
+        mSecurityInfo = (RegularHexagonView) findViewById(R.id.rhv_security_informatization);
+        mRuralInfo = (RegularHexagonView) findViewById(R.id.rhv_rural_informatization);
+        mMoveWork = (RegularHexagonView) findViewById(R.id.rhv_move_working);
+        mHotelInfo = (RegularHexagonView) findViewById(R.id.rhv_hotel_informatization);
+        mFinanceInfo = (RegularHexagonView) findViewById(R.id.rhv_financial_informatization);
+        mLibInfo = (RegularHexagonView) findViewById(R.id.rhv_library_cloud);
+        mIndustryInfo = (RegularHexagonView) findViewById(R.id.rhv_industry_informatization);
 
         methodLeft.setOnClickListener(new myListener());
         methodTitle.setOnClickListener(new myListener());
         methodIcon.setOnClickListener(new myListener());
-
     }
 
-    private class myListener implements View.OnClickListener{
+    private class myListener implements View.OnClickListener {
         @Override
-        public void onClick(View v){
-            switch (v.getId()){
-                case R.id.iv_methodLeft:
-                case R.id.iv_methonIcon:
-                case R.id.mtv_methodTitle:
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.iv_method_left:
+                case R.id.iv_method_left_icon:
+                case R.id.tv_method_title:
                     Intent intent = new Intent(MainActivity.ACTION_MAINACTIVITY);
                     intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                     MethodActivity.this.startActivity(intent);
                     finish();
                     break;
-                case R.id.rl_smartCamp:
+                case R.id.rl_smart_campus:
                     break;
-                case R.id.rl_smartComp:
+                case R.id.rl_smart_company:
                     break;
-                case R.id.rhv_finaInfo:
+                case R.id.rhv_financial_informatization:
                     break;
-                case R.id.rhv_holtInfo:
+                case R.id.rhv_hotel_informatization:
                     break;
-                case R.id.rhv_induInfo:
+                case R.id.rhv_industry_informatization:
                     break;
-                case R.id.rhv_infoSafe:
+                case R.id.rhv_security_informatization:
                     break;
-                case R.id.rhv_libiInfo:
+                case R.id.rhv_library_cloud:
                     break;
-                case R.id.rhv_moveWork:
+                case R.id.rhv_move_working:
                     break;
-                case R.id.rhv_vallInfo:
+                case R.id.rhv_rural_informatization:
                     break;
 
             }
